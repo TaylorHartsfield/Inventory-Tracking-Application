691e12d805828300a41d4ff63cf1e89c916715ca
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-15 21:24:11 -0600

    Build Inventory Class and set home page route

--- inventory_app/server.py
+++ inventory_app/server.py
@@ -9,5 +9,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Inventory(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000), nullable=False)
+    count = db.Column(db.Integer)
+    date_posted = db.Column(db.DateTime, default=datetime.now())
+    shipment = db.Column(db.String(1000))
+
+    def __init__(self, name, count, date_posted, shipment="Not Assigned"):
+        self.name = name
+        self.count = count
+        self.date = date_posted
+        self.shipment = shipment
+        
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
