a8c46934a038a1d5e9a458f22d2fe89cb3221fb8
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-15 21:22:56 -0600

    Initialize Database

--- inventory_app/server.py
+++ inventory_app/server.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.secret_key = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
